fb5d0ed042b3d8956a1471519871c54fbebe7528
Ahmed Mohamed <77761414+ahmedmk323@users.noreply.github.com>
2024-11-25 15:41:15 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,7 +3,7 @@ package SimpleJavaProgram;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Made some changes");
 	}
 
 }
